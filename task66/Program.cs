96a5887eeebb9c88769f162444e08e2fc06f3826
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-15 15:55:54 +0500

    task66_nutsum

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void NatNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    NatNumberSum(numberM, numberN, sum);
+}
+
+NatNumberSum(numberM, numberN, 0);
