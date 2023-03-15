7c43309bf6e73032fb7269de94ca4e73a3aa27f5
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-15 16:04:09 +0500

    task66_nutsum

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,10 +2,10 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.WriteLine("Введите начальное число M:");
+Console.Write("Введите начальное число M: ");
 int numberM = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите начальное число N:");
+Console.Write("Введите начальное число N: ");
 int numberN = int.Parse(Console.ReadLine());
 
 ///Метод нахождения суммы натуральных элементов в промежутке от M до N
